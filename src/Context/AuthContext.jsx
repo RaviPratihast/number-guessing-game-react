000f95bf763666e2e4aabc5e459b11809dbc3e22
Ravi Pratihast <ravipratihast@gmail.com>
2024-09-13 23:01:53 +0530

    Add: setup of authentication context.

---
+++ src/Context/AuthContext.jsx
@@ -0,0 +1,26 @@
+import { createContext, useContext, useReducer } from "react";
+import authenticationReducer from "../Reducers/AuthenticationReducer";
+
+const AuthContext = createContext();
+const useAuth = () => useContext(AuthContext);
+
+const initialState = {
+  loggedIn: false,
+  users: [
+    { user: "Ravi", password: "123" },
+    { user: "a", password: "1" },
+  ],
+};
+
+const AuthProvider = ({ children }) => {
+  const [stateAuth, dispatchAuth] = useReducer(
+    authenticationReducer,
+    initialState
+  );
+  return (
+    <AuthContext.Provider value={{ stateAuth, dispatchAuth }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+export { useAuth, AuthProvider };
