78b0c0be31de6e5ca82914823b4ad261de786839
Ravi Pratihast <ravipratihast@gmail.com>
2024-09-13 23:07:20 +0530

    chore: wrap App component with AuthProvider for authentication context.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import { AuthProvider } from "./Context/AuthContext.jsx";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <Router>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </Router>
 );
